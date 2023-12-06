415a701fd3b9bbb64865e06396d2fd6727b90e97
Patrick Heil <pawnxpawnofficial+github@gmail.com>
2023-12-06 13:28:31 -0700

    Smooth
    +Fixed animations
    +Added rotation of character

--- Assets/Scripts/Player/PayerBasicMovement.gd
+++ Assets/Scripts/Player/PayerBasicMovement.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+var turn_speed = 10
 var  facing_right:bool = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -26,13 +27,9 @@ func _physics_process(delta):
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
+		$TestPlayerV2.rotation.y = lerp_angle($TestPlayerV2.rotation.y, atan2(direction.x, direction.z), delta * turn_speed)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	if direction.x < 0 && facing_right:
-		flip()
-	if direction.x > 0 && !facing_right:
-		flip()
 	
 	$AnimationTree.set("parameters/conditions/idle", input_dir == Vector2.ZERO && is_on_floor())
 	$AnimationTree.set("parameters/conditions/walk", input_dir != Vector2.ZERO && is_on_floor())
@@ -41,7 +38,3 @@ func _physics_process(delta):
 	$AnimationTree.set("parameters/conditions/jump",  Input.is_action_just_pressed("ui_accept") && is_on_floor())
 
 	move_and_slide()
-
-func flip():
-	$TestPlayerV2.rotation_degrees.y *= -1
-	facing_right = !facing_right
