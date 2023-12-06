80f522466e03b23b31f8111e41cded3d45fac3f4
Patrick Heil <pawnxpawnofficial+github@gmail.com>
2023-12-05 23:24:01 -0700

    quick update
    + Quit function
    + Rotation

--- Assets/Scripts/Player/PayerBasicMovement.gd
+++ Assets/Scripts/Player/PayerBasicMovement.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+var  facing_right:bool = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -13,19 +14,25 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "na", "na")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	if direction.x < 0 && facing_right:
+		flip()
+	if direction.x > 0 && !facing_right:
+		flip()
 	
 	$AnimationTree.set("parameters/conditions/idle", input_dir == Vector2.ZERO && is_on_floor())
 	$AnimationTree.set("parameters/conditions/walk", input_dir != Vector2.ZERO && is_on_floor())
@@ -34,3 +41,7 @@ func _physics_process(delta):
 	$AnimationTree.set("parameters/conditions/jump",  Input.is_action_just_pressed("ui_accept") && is_on_floor())
 
 	move_and_slide()
+
+func flip():
+	$TestPlayerV2.rotation_degrees.y *= -1
+	facing_right = !facing_right
