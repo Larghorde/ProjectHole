73d712a1f019d7ad8b519036d14879962c9f6e89
Patrick Heil <pawnxpawnofficial+github@gmail.com>
2023-12-06 14:14:58 -0700

    Minor movement control fix
    Changed how movement worked

--- Assets/Scripts/Player/PayerBasicMovement.gd
+++ Assets/Scripts/Player/PayerBasicMovement.gd
@@ -3,8 +3,10 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-var turn_speed = 10
+@export var turn_speed = 10
 var  facing_right:bool = false
+
+@onready var axis = Vector3.ZERO 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -23,16 +25,16 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "na", "na")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	axis.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+	var direction = (transform.basis * Vector3(axis.x, 0, 0)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		$TestPlayerV2.rotation.y = lerp_angle($TestPlayerV2.rotation.y, atan2(direction.x, direction.z), delta * turn_speed)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	$AnimationTree.set("parameters/conditions/idle", input_dir == Vector2.ZERO && is_on_floor())
-	$AnimationTree.set("parameters/conditions/walk", input_dir != Vector2.ZERO && is_on_floor())
+	$AnimationTree.set("parameters/conditions/idle", axis.x == 0 && is_on_floor())
+	$AnimationTree.set("parameters/conditions/walk", axis.x != 0 && is_on_floor())
 	$AnimationTree.set("parameters/conditions/falling", !is_on_floor())
 	$AnimationTree.set("parameters/conditions/landed", is_on_floor())
 	$AnimationTree.set("parameters/conditions/jump",  Input.is_action_just_pressed("ui_accept") && is_on_floor())
